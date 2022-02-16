718edc392b1c93ff2c00cb62a0603da18579ebb1
Amethyst Willia <zmithic@gmail.com>
2022-02-16 17:37:19 -0600

    findById now sorts by step id

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -28,6 +28,7 @@ async function findById(scheme_id) { // EXERCISE B
   const result = await db('schemes as sc')
     .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
     .select('sc.scheme_id', 'scheme_name', 'step_id', 'step_number', 'instructions')
+    .orderBy('step_number')
     .where('sc.scheme_id', scheme_id);
 
   const scheme = {
